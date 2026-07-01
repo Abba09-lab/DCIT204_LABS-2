8d80dbd8ef4a2f62334db0c2cf409e303285f35b
Abba09-lab <amandoh002@st.ug.edu.gh>
2026-07-01 07:08:18 +0000

    Create FindMax.java

---
+++ Lab2/FindMax.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class FindMax {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element at index " + i + ": ");
+            arr[i] = scanner.nextInt();
+        }
+
+        int maxValue = arr[0];
+        int maxIndex = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+                maxIndex = i;
+            }
+        }
+
+        System.out.println("\n--- Result ---");
+        System.out.println("Array: " + java.util.Arrays.toString(arr));
+        System.out.println("Maximum value: " + maxValue);
+        System.out.println("Found at index: " + maxIndex);
+
+        scanner.close();
+    }
+}
